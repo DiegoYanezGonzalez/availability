e91a3412559203697f46a09ac89706deaba2cb5d
Diego Ignacio Yañez Gonzalez <extdyanez@transbank.cl>
2025-07-08 23:00:54 -0400

    Add API, integration tests and GitHub Actions CI

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -11,7 +11,8 @@ def test_form_submission():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_options)
+    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=options)
+
 
     driver.get("http://127.0.0.1:5000")
 
